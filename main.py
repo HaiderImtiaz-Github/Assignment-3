eef3877397e290035536181edefbb49e62e54625
Haider Imtiaz <haider.imtiaz38@ee.ceme.edu.pk>
2024-09-24 23:59:29 +0500

    Completed

--- main.py
+++ main.py
@@ -1,41 +1,41 @@
 import random
 import time
 
-
 BOARD = [" "] * 9
 PLAYER = "X"
 
 
-def print_board(board):
+def print_board():
     print("\n")
-    print(f"{board[0]} | {board[1]} | {board[2]}")
+    print(f"{BOARD[0]} | {BOARD[1]} | {BOARD[2]}")
     print("--|---|--")
-    print(f"{board[3]} | {board[4]} | {board[5]}")
+    print(f"{BOARD[3]} | {BOARD[4]} | {BOARD[5]}")
     print("--|---|--")
-    print(f"{board[6]} | {board[7]} | {board[8]}")
+    print(f"{BOARD[6]} | {BOARD[7]} | {BOARD[8]}")
     print("\n")
-
 
 
 def player_move():
+    global PLAYER
     print(f"Player {PLAYER}'s Turn.")
-    move = int("Enter Place  between 1-9 : ")
-    if BOARD[move]==" ":
+    move = int(input("Enter place between 1-9: ")) - 1  # Subtract 1 to match list indexing
+    if BOARD[move] == " ":
         BOARD[move] = PLAYER
     else:
         print("This spot is already taken! Try again.")
         player_move()
 
+
 def ai_move():
     print("Player O's Turn.")
     time.sleep(2)
-    move = random.randint(0,9)
-    if BOARD[move]==" ":
+    move = random.randint(0, 8)  # Correct range to 0-8
+    if BOARD[move] == " ":
         BOARD[move] = "O"
     else:
-        print("This spot is already taken! Try again.")
         ai_move()
-    
+
+
 def check_winner(board, mark):
     win_conditions = [(0, 1, 2), (3, 4, 5), (6, 7, 8),  # rows
                       (0, 3, 6), (1, 4, 7), (2, 5, 8),  # columns
@@ -45,10 +45,10 @@ def check_winner(board, mark):
             return True
     return False
 
+
 def board_full():
-    if " " not in BOARD:
-        return True
-    return False
+    return " " not in BOARD
+
 
 def tic_tac_toe():
     game_running = True
@@ -56,22 +56,35 @@ def tic_tac_toe():
     while game_running:
         print_board()
         player_move()
-        ai_move()
         if check_winner(BOARD, "X"):
-            print("Player X has won")
+            print_board()
+            print("Player X has won!")
             game_running = False
-        elif check_winner(BOARD, "O"):
-            print("Player O has won")
+            break
+        if board_full():
+            print_board()
+            print("It's a draw!")
             game_running = False
-        elif board_full():
-            print("It's a draw")
+            break
+
+        ai_move()
+        if check_winner(BOARD, "O"):
+            print_board()
+            print("Player O has won!")
             game_running = False
-        
+            break
+        if board_full():
+            print_board()
+            print("It's a draw!")
+            game_running = False
+            break
+
+
 print('''  1 | 2 | 3
- -----------
+ ----------- 
   4 | 5 | 6
  -----------
   7 | 8 | 9
 ''')
-print("Welcome to Haider's Tic Tac Toe Assigment")
+print("Welcome to Haider's Tic Tac Toe Assignment")
 tic_tac_toe()
